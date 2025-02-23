c453b4257f1d8d9dc00e011cf5cf019b5fd94a0d
K2s <elieserhs999omega@gmail.com>
2025-02-23 11:52:23 -0500

    Finish

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CV_Pro/',
   build: {
-    // base: '/CV_Pro/',
     chunkSizeWarningLimit: 10000, // Ajusta el límite según sea necesario
   },
 })
